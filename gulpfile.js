21444e6c8a8e02ff026d7765a67b0f9d30797ee1
Adrian Korzan <adrian.korzan@gmail.com>
2019-09-11 01:16:10 +0200

    Portfolio 1.0.0

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,7 @@ const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
 const cleanCSS = require('gulp-clean-css');
 const sourcemaps = require('gulp-sourcemaps');
-
-
+const minify = require('gulp-minify');
 
 
 
@@ -18,8 +17,15 @@ gulp.task('sass', () =>{
 	.pipe(gulp.dest("./css"));
 });
 
+gulp.task('minify', () => {
+	return gulp.src('./js/main.js', { allowEmpty: true }) 
+	  .pipe(minify({noSource: true}))
+	  .pipe(gulp.dest('./js'))
+  })
+
 gulp.task("watch",()=>{
 	gulp.watch("./scss/**/*.scss", gulp.series("sass"));
+	gulp.watch("./js/main.js", gulp.series("minify"));
 });
 
 gulp.task('default', gulp.parallel('watch')); //funkcja defaultowa
--- js/main.js
+++ js/main.js
@@ -53,10 +53,8 @@ const activeCllasToNav = () =>{
 	let sectionContact = document.getElementById('contact');
 	let navContact = document.querySelector("a.nav-link[href='#contact']");
 	
-	// listen for scroll event and call animate function
 	document.addEventListener('scroll', animate);
 	
-	// check if element is in view
 	function inView(element) {
 	  var elementHeight = element.clientHeight;
 	  var windowHeight = window.innerHeight;
@@ -65,7 +63,6 @@ const activeCllasToNav = () =>{
 	  var scrollPosition = scrollY + windowHeight;
 	  var elementPosition = element.getBoundingClientRect().top + scrollY + elementHeight/2;
 	  
-	  // is scroll position greater than element position? (is element in view?)
 	  if (scrollPosition >= elementPosition && scrollPosition <= element.getBoundingClientRect().bottom + scrollY + elementHeight) {
 		return true;
 	  }
@@ -73,32 +70,26 @@ const activeCllasToNav = () =>{
 	  return false;
 	}
 	
-	// animate element when it is in view
 	function animate() {
-	  // is element in view?
 	  if (inView(sectionHome)) {
-		  // element is in view, add class to element	  
 		  navHome.classList.add("active");
 	  }
 	  else{
 		  navHome.classList.remove("active");
 	  }
 	  if (inView(sectionAbout)) {
-		  // element is in view, add class to element	  
 		  navAbout.classList.add("active");
 	  }
 	  else{
 		  navAbout.classList.remove("active");
 	  }
 	  if (inView(sectionPortfolio)) {
-		  // element is in view, add class to element	  
 		  navPortfolio.classList.add("active");
 	  }
 	  else{
 		  navPortfolio.classList.remove("active");
 	  }
 	  if (inView(sectionContact)) {
-		  // element is in view, add class to element	  
 		  navContact.classList.add("active");
 	  }
 	  else{
@@ -112,27 +103,19 @@ activeCllasToNav();
 
 
 // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! ANIMACJE !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-// get the element to animate
+let animations = () =>{
 let myselfDesc = document.getElementById('myself-desc');
 let skills = document.getElementById('skills');
-// let elementHeight = element.clientHeight;
 
-// listen for scroll event and call animate function
 document.addEventListener('scroll', animate);
 
-// check if element is in view
 function inView(element) {
-  // get window height
   var windowHeight = window.innerHeight;
-  // get number of pixels that the document is scrolled
   var scrollY = window.scrollY || window.pageYOffset;
   
-  // get current scroll position (distance from the top of the page to the bottom of the current viewport)
   var scrollPosition = scrollY + windowHeight;
-  // get element position (distance from the top of the page to the bottom of the element)
   var elementPosition = element.getBoundingClientRect().top + scrollY;
   
-  // is scroll position greater than element position? (is element in view?)
   if (scrollPosition > elementPosition) {
     return true;
   }
@@ -140,16 +123,14 @@ function inView(element) {
   return false;
 }
 
-// animate element when it is in view
 function animate() {
-  // is element in view?
   if (inView(myselfDesc)) {
-      // element is in view, add class to element
       myselfDesc.classList.add('bounce');
   }
   if (inView(skills)) {
-      // element is in view, add class to element
       skills.classList.add('swing');
   }
 }
+}
+animations();
 // END ANIMACJE
